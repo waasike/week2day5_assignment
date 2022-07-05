923f4bfacaf80f652c20590a2683f99acccf319b
waasike <mitchellewasikee@gmail.com>
2022-07-05 15:54:07 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#constructing a pattern using a nested loop
+n = 5
+
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+
+    print('')
+
+for i in range(n,0,-1):
+
+    for j in range(i): 
+
+       print('* ', end="")
+
+    print('')
